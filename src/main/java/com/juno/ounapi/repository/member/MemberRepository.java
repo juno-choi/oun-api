e3c588e5fcf93f007a5280d12ead647963a8bd56
juno-choi <ililil9482@naver.com>
2022-10-10 14:12:53 +0900

    [추가] 회원가입 로직 단위 테스트 코드 추가

--- src/main/java/com/juno/ounapi/repository/member/MemberRepository.java
+++ src/main/java/com/juno/ounapi/repository/member/MemberRepository.java
@@ -2,11 +2,12 @@ package com.juno.ounapi.repository.member;
 
 import com.juno.ounapi.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends CrudRepository<Member, Long> {
     Optional<Member> findByMemberId(String memberId);
 }
--- src/test/java/com/juno/ounapi/service/kakao/OauthServiceImplUnitTest.java
+++ src/test/java/com/juno/ounapi/service/kakao/OauthServiceImplUnitTest.java
@@ -3,8 +3,10 @@ package com.juno.ounapi.service.kakao;
 import com.juno.ounapi.common.httpclient.MyHttpClient;
 import com.juno.ounapi.domain.member.Member;
 import com.juno.ounapi.dto.kakao.OauthRequest;
+import com.juno.ounapi.enums.oauth.Oauth;
 import com.juno.ounapi.exception.CommonException;
 import com.juno.ounapi.repository.member.MemberRepository;
+import com.juno.ounapi.vo.kakao.OauthResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -89,7 +91,7 @@ class OauthServiceImplUnitTest {
         CommonException ex = assertThrows(CommonException.class, () -> oauthService.oauthToken(request));
 
         // then
-        assertEquals("회원 조회 실패", ex.getMessage());
+        assertEquals("LOGIN_FAIL", ex.getMessage());
     }
 
 
@@ -140,13 +142,72 @@ class OauthServiceImplUnitTest {
                         return HttpClient.Version.HTTP_1_1;
                     }
                 });
-        Member member = new Member(1L, "kakao", "123456", null, null, "최준호", null, null, "profile image", "thumbnail image", LocalDateTime.now(), LocalDateTime.now());
+        Member member = new Member(1L, Oauth.KAKAO.name(), "123456", "ililil9482@naver.com", null, "최준호", null, null, "profile image", "thumbnail image", LocalDateTime.now(), LocalDateTime.now());
         given(memberRepository.findByMemberId(any())).willReturn(Optional.of(member));
 
         // when
-        oauthService.oauthToken(request);
+        OauthResponse oauthResponse = oauthService.oauthToken(request);
 
         // then
+        assertEquals(member.getEmail(), oauthResponse.getEmail());
+    }
+
+
+    @Test
+    @DisplayName("회원가입에 성공한다.")
+    void oauthJoinSuccess(){
+        // given
+        OauthRequest request = new OauthRequest("access_token", "bearer", "refresh_token", 21599L, "account_email profile_image profile_nickname", 5183999L);
+        given(myHttpClient.httpPostRequest(any()))
+                .willReturn(new HttpResponse<String>() {
+                    @Override
+                    public int statusCode() {
+                        return 200;
+                    }
+
+                    @Override
+                    public HttpRequest request() {
+                        return null;
+                    }
+
+                    @Override
+                    public Optional<HttpResponse<String>> previousResponse() {
+                        return Optional.empty();
+                    }
+
+                    @Override
+                    public HttpHeaders headers() {
+                        return null;
+                    }
+
+                    @Override
+                    public String body() {
+                        return "{\"id\":2469559669,\"connected_at\":\"2022-10-03T06:24:11Z\",\"properties\":{\"nickname\":\"최준호\",\"profile_image\":\"http://k.kakaocdn.net/dn/bHdRC1/btrNQSBajfI/cFJiaU9WnODens10jJXdOK/img_640x640.jpg\",\"thumbnail_image\":\"http://k.kakaocdn.net/dn/bHdRC1/btrNQSBajfI/cFJiaU9WnODens10jJXdOK/img_110x110.jpg\"},\"kakao_account\":{\"profile_nickname_needs_agreement\":false,\"profile_image_needs_agreement\":false,\"profile\":{\"nickname\":\"최준호\",\"thumbnail_image_url\":\"http://k.kakaocdn.net/dn/bHdRC1/btrNQSBajfI/cFJiaU9WnODens10jJXdOK/img_110x110.jpg\",\"profile_image_url\":\"http://k.kakaocdn.net/dn/bHdRC1/btrNQSBajfI/cFJiaU9WnODens10jJXdOK/img_640x640.jpg\",\"is_default_image\":false},\"has_email\":true,\"email_needs_agreement\":false,\"is_email_valid\":true,\"is_email_verified\":true,\"email\":\"ililil9482@naver.com\"}}";
+                    }
+
+                    @Override
+                    public Optional<SSLSession> sslSession() {
+                        return Optional.empty();
+                    }
+
+                    @Override
+                    public URI uri() {
+                        return null;
+                    }
+
+                    @Override
+                    public HttpClient.Version version() {
+                        return HttpClient.Version.HTTP_1_1;
+                    }
+                });
+        Member member = new Member(1L, Oauth.KAKAO.name(), "123456", "ililil9482@naver.com", null, "최준호", null, null, "profile image", "thumbnail image", LocalDateTime.now(), LocalDateTime.now());
+        given(memberRepository.save(any())).willReturn(member);
+
+        // when
+        OauthResponse oauthResponse = oauthService.oauthJoin(request);
+
+        // then
+        assertEquals(member.getEmail(), oauthResponse.getEmail());
 
     }
 }
