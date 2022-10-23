47102903b6217f4444138af66ca6cd650defcfb8
juno-choi <ililil9482@naver.com>
2022-10-23 18:21:24 +0900

    [기타] 실제 문서를 작업하여 test 용도로 작업했던 문서부분 제거

--- src/main/java/com/juno/ounapi/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.juno.ounapi.service;
-
-import com.juno.ounapi.dto.TestDto;
-import com.juno.ounapi.vo.TestResponse;
-
-public interface TestService {
-    TestResponse test(TestDto testDto);
-}
